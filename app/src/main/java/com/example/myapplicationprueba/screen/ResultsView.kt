a6764b7947e2cdf1527cd3b78576d266bf236df0
Pau0911 <paulaa.gomez@udea.edu.co>
2022-10-16 00:02:44 -0500

    Implementación de listado con cards

--- app/src/main/java/com/example/myapplicationprueba/screen/ResultsView.kt
+++ app/src/main/java/com/example/myapplicationprueba/screen/ResultsView.kt
@@ -1,19 +1,29 @@
 package com.example.myapplicationprueba
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
@@ -21,6 +31,7 @@ import androidx.paging.PagingData
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.myapplicationprueba.model.Product
 import com.example.myapplicationprueba.screen.state.ErrorItem
@@ -53,7 +64,9 @@ fun ProductList(products: Flow<PagingData<Product>>) {
     LazyColumn {
 
         items(lazyProductItems) { product ->
-            ProductItem(product = product!!)
+            ProductItem(product = product!!, onClick = {
+
+            })
         }
 
         lazyProductItems.apply {
@@ -85,22 +98,67 @@ fun ProductList(products: Flow<PagingData<Product>>) {
 
 
 @Composable
-fun ProductItem(product: Product) {
-    Row(
+fun ProductItem(product: Product,onClick: () -> Unit) {
+    Card(
         modifier = Modifier
-            .padding(start = 16.dp, top = 16.dp, end = 16.dp)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+            .padding(
+                bottom = 5.dp, top = 5.dp,
+                start = 5.dp, end = 5.dp
+            )
+            .fillMaxWidth()
+            .clickable(onClick = onClick),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 12.dp
     ) {
-        ProductTitle(
-            product.title!!, modifier = Modifier.weight(1f)
-        )
-        ProductImage(
-            product.thumbnail, modifier = Modifier
-                .padding(start = 16.dp)
-                .size(90.dp)
-        )
+        Row(
+            modifier = Modifier
+                .clip(RoundedCornerShape(4.dp))
+                .background(MaterialTheme.colors.surface)
+        ) {
+            Surface(
+                modifier = Modifier.size(130.dp),
+                shape = RoundedCornerShape(12.dp),
+                color = MaterialTheme.colors.surface.copy(
+                    alpha = 0.2f
+                )
+            ) {
+                val painter= rememberAsyncImagePainter(product.thumbnail)
+                Image( painter = painter,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(100.dp)
+                        .clip(shape = RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Crop )
+//                ProductImage(product.thumbnail,modifier = Modifier
+//                    .padding(start = 16.dp)
+//                    .size(90.dp)
+//                )
+
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 12.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = product.title,
+                    fontWeight = FontWeight.Bold,
+                    style = TextStyle(fontSize = 22.sp),
+                    color = Color.Black
+                )
+                CompositionLocalProvider(
+                    LocalContentAlpha provides ContentAlpha.medium
+                ) {
+                    Text(
+                        text = product.price.toString(),
+                        style = typography.body2,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        modifier = Modifier.padding(end = 25.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
